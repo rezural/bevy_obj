15e3c1671131d20e5d55beff6b0cad51ed29795e
rezural <rezural@protonmail.com>
2021-02-03 01:53:43 +1100

    loading from a bare obj file working, but normals are inversed

--- src/loader.rs
+++ src/loader.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
+use bevy::prelude::Vec3;
 use bevy_asset::{AssetLoader, LoadContext, LoadedAsset};
 use bevy_render::{
     mesh::{Indices, Mesh, VertexAttributeValues},
     pipeline::PrimitiveTopology,
 };
 use bevy_utils::BoxedFuture;
+use obj::Vertex;
 use thiserror::Error;
 
 #[derive(Default)]
@@ -30,8 +32,8 @@ pub enum ObjError {
     #[error("Invalid OBJ file.")]
     Gltf(#[from] obj::ObjError),
 }
-
 async fn load_obj<'a, 'b>(
+
     bytes: &'a [u8],
     load_context: &'a mut LoadContext<'b>,
 ) -> Result<(), ObjError> {
@@ -41,17 +43,11 @@ async fn load_obj<'a, 'b>(
     Ok(())
 }
 
-fn load_obj_pnt(obj: obj::Obj<obj::TexturedVertex>, mesh: &mut Mesh) {
-    let uvs = VertexAttributeValues::Float3(
-        obj.vertices
-            .iter()
-            // Flip UV for correct values
-            .map(|v| [v.texture[0], 1.0 - v.texture[1], v.texture[2]])
-            .collect(),
-    );
+fn load_obj_pnt(obj: obj::Obj<obj::Position, u32>, mesh: &mut Mesh) {
     let positions =
         VertexAttributeValues::Float3(obj.vertices.iter().map(|v| v.position).collect());
-    let normals = VertexAttributeValues::Float3(obj.vertices.iter().map(|v| v.normal).collect());
+    let normals =VertexAttributeValues::Float3( normals_for_positions(&obj));
+    let uvs = uvs_for_positions(&obj);
 
     mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
     mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
@@ -60,8 +56,81 @@ fn load_obj_pnt(obj: obj::Obj<obj::TexturedVertex>, mesh: &mut Mesh) {
     set_mesh_indices(mesh, obj);
 }
 
-fn set_mesh_indices<T>(mesh: &mut Mesh, obj: obj::Obj<T>) {
+fn uvs_for_positions(positions: &obj::Obj<obj::Position, u32>) -> Vec<[f32; 3]> {
+
+    let uvs = positions
+        .vertices
+        .iter()
+        .map(|_| [0.; 3])
+        .collect();
+
+    uvs
+}
+
+fn normals_for_positions(positions: &obj::Obj<obj::Position, u32>) -> Vec<[f32; 3]> {
+    let vertices = positions.vertices.clone();
+    let indexes = positions.indices.clone();
+    let mut normals: Vec<Vec3> = Vec::new();
+    let zero = Vec3::new(0., 0., 0.);
+    for _ in &vertices {
+        normals.push(zero);
+    }
+    for indexes in indexes.windows(3) {
+        let v0 = vertices.get(indexes[0] as usize).unwrap();
+        let v1 = vertices.get(indexes[1] as usize).unwrap();
+        let v2 = vertices.get(indexes[2] as usize).unwrap();
+        let v0 = bevy_vec3_from_position(v0);
+        let v1 = bevy_vec3_from_position(v1);
+        let v2 = bevy_vec3_from_position(v2);
+        //     let normal = (v0 - v1).cross(&(v2 - v1));
+        let normal: Vec3 = (v0 - v1).cross(v2 - v1);
+        // let normal = Vec3::new(-normal.x, -normal.y, -normal.z);
+        let normal = normal.normalize();
+        *normals.get_mut(indexes[0] as usize).unwrap() += normal;
+    }
+
+    normals
+        .iter()
+        .map(|normal| {
+            let mut slice = [0.; 3];
+            normal.write_to_slice_unaligned(&mut slice);
+            slice
+        })
+        .collect()
+}
+
+#[inline]
+fn bevy_vec3_from_position(position: &obj::Position) -> Vec3 {
+    Vec3::from(position.position)
+    // Vec3::new(position.position[0], position.position[1], position.position[2])
+}
+
+fn set_mesh_indices<T>(mesh: &mut Mesh, obj: obj::Obj<T, u32>) {
     mesh.set_indices(Some(Indices::U32(
         obj.indices.iter().map(|i| *i as u32).collect(),
     )));
 }
+    // fn normals_from_positions_mesh(positions: VertexAttributeValues) -> VertexAttributeValues {
+
+        // match positions {
+        //     VertexAttributeValues::Float3(vec) => {
+        //         let v1 = vec[0];
+        //         let v2 = vec[1];
+        //         let v3 = vec[2];
+
+        //     }
+        //     _ => panic!("unimplemented")
+        // }
+        // assert_eq!(normal_directions.len(), self.vertices.len());
+
+        // for tri_verts in self.triangles.iter() {
+        //     let v0 = &self.vertices[tri_verts[0]];
+        //     let v1 = &self.vertices[tri_verts[1]];
+        //     let v2 = &self.vertices[tri_verts[2]];
+        //     let normal = (v0 - v1).cross(&(v2 - v1));
+
+        //     normal_directions[tri_verts[0]] += normal;
+        //     normal_directions[tri_verts[1]] += normal;
+        //     normal_directions[tri_verts[2]] += normal;
+        // }
+    // }
